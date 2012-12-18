5a7725ff0a294131cac650783174297546dd7295
Thomas Jarrand <thomas.jarrand@gmail.com>
2012-12-18 18:07:05 +0100

    Integration contre natuuuuraaaan

--- src/Tom32i/SiteBundle/Form/SecretType.php
+++ src/Tom32i/SiteBundle/Form/SecretType.php
@@ -11,7 +11,7 @@ class SecretType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('content')
+            ->add('content', null, array('required' => true))
         ;
     }
 
